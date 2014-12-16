409dcd7177cf6f3468ba0e4358b79ded6d39aa7a
qbi11y <qbi11y@gmail.com>
2014-12-16 10:46:03 -0600

    12-16-2014-1045

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,21 @@ var app = angular.module('cmo', []);
 app.factory('Products', function() {
     var products = [
     {
-        name: 'Service Name 1',
+        name: 'Amazon',
         description: 'Lorem ipsum dolor sit amet, sea id molestie eleifend moderatius, ubique mandamus nec ex. Pro error mentitum ad, pri sonet facilisis voluptatum in, pri voluptua partiendo intellegebat eu.',
         price: 50.00,
         thumbnail: '',
         points: 1000
     },
     {
-        name: 'Service Name 2',
+        name: 'Azure',
         description: 'Lorem ipsum dolor sit amet, sea id molestie eleifend moderatius, ubique mandamus nec ex. Pro error mentitum ad, pri sonet facilisis voluptatum in, pri voluptua partiendo intellegebat eu.',
         price: 50.00,
         thumbnail: '',
         points: 1000
     },
     {
-        name: 'Service Name 3',
+        name: 'Cloudera',
         description: 'Lorem ipsum dolor sit amet, sea id molestie eleifend moderatius, ubique mandamus nec ex. Pro error mentitum ad, pri sonet facilisis voluptatum in, pri voluptua partiendo intellegebat eu.',
         price: 50.00,
         thumbnail: '',
