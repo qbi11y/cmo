994851a6d2c4f0ea97df46829c7448e37d3b4aa0
qbi11y <qbi11y@gmail.com>
2014-12-16 18:24:16 -0600

    12-16-2014-18:23
    added store interactivity

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,124 @@
 var app = angular.module('cmo', []);
 
+//holds the similar products
+app.factory('SimilarProducts', function() {
+    var similarProducts = [
+    {
+        provider: 'Google',
+        price: 100,
+        thumbnail: 'http://res.cloudinary.com/gravitant/image/upload/v1413466762/providers/google.png',
+        points: 1500
+    },
+    {
+        provider: 'Azure',
+        price: 90,
+        thumbnail: 'http://res.cloudinary.com/gravitant/image/upload/v1413466762/providers/azure.png',
+        points: 1750
+    },
+    {
+        provider: 'OpenStack',
+        price: 110,
+        thumbnail: 'http://res.cloudinary.com/gravitant/image/upload/v1413466762/providers/openstack.png',
+        points: 500
+    }];
+
+    return similarProducts
+});
+
+
+//holds all the products
 app.factory('Products', function() {
     var products = [
     {
-        name: 'Amazon',
+        name: 'Amazon Web Services',
         description: 'Lorem ipsum dolor sit amet, sea id molestie eleifend moderatius, ubique mandamus nec ex. Pro error mentitum ad, pri sonet facilisis voluptatum in, pri voluptua partiendo intellegebat eu.',
-        price: 50.00,
-        thumbnail: '',
-        points: 1000
+        shortDescription: 'Lorem ipsum dolor sit amet, sea id molestie eleifend moderatius, ubique mandamus nec ex.',
+        price: 150.00,
+        thumbnail: 'http://res.cloudinary.com/gravitant/image/upload/v1413466761/providers/aws.jpg',
+        points: 1500,
+        categories: [{
+            name: 'Category -- 1',
+            options: [{
+                name: 'Some Name',
+                value: 'Some Value'
+            },
+            {
+                name: 'Another Name',
+                value: 'Another Value',
+            },
+            {
+                name: 'Last Name',
+                value: 'Last Value'
+            }]
+        },
+        {
+            name: 'Category -- 2',
+            options: [{
+                name: 'Some Name',
+                value: 'Some Value'
+            },
+            {
+                name: 'Another Name',
+                value: 'Another Value',
+            },
+            {
+                name: 'Last Name',
+                value: 'Last Value'
+            }]
+        },
+        {
+            name: 'Category -- 3',
+            options: [{
+                name: 'Some Name',
+                value: 'Some Value'
+            },
+            {
+                name: 'Another Name',
+                value: 'Another Value',
+            },
+            {
+                name: 'Last Name',
+                value: 'Last Value'
+            }]
+        }]
     },
     {
         name: 'Azure',
         description: 'Lorem ipsum dolor sit amet, sea id molestie eleifend moderatius, ubique mandamus nec ex. Pro error mentitum ad, pri sonet facilisis voluptatum in, pri voluptua partiendo intellegebat eu.',
-        price: 50.00,
-        thumbnail: '',
+        shortDescription: 'Lorem ipsum dolor sit amet, sea id molestie eleifend moderatius, ubique mandamus nec ex.',
+        price: 500.00,
+        thumbnail: 'http://res.cloudinary.com/gravitant/image/upload/v1413466762/providers/azure.png',
+        points: 1250
+    },
+    {
+        name: 'OpenStack',
+        description: 'Lorem ipsum dolor sit amet, sea id molestie eleifend moderatius, ubique mandamus nec ex. Pro error mentitum ad, pri sonet facilisis voluptatum in, pri voluptua partiendo intellegebat eu.',
+        shortDescription: 'Lorem ipsum dolor sit amet, sea id molestie eleifend moderatius, ubique mandamus nec ex.',
+        price: 750.00,
+        thumbnail: 'http://res.cloudinary.com/gravitant/image/upload/v1413466762/providers/openstack.png',
         points: 1000
     },
     {
-        name: 'Cloudera',
+        name: 'HP',
         description: 'Lorem ipsum dolor sit amet, sea id molestie eleifend moderatius, ubique mandamus nec ex. Pro error mentitum ad, pri sonet facilisis voluptatum in, pri voluptua partiendo intellegebat eu.',
-        price: 50.00,
-        thumbnail: '',
+        shortDescription: 'Lorem ipsum dolor sit amet, sea id molestie eleifend moderatius, ubique mandamus nec ex.',
+        price: 0.00,
+        thumbnail: 'http://res.cloudinary.com/gravitant/image/upload/v1413466762/providers/hpcloud.png',
+        points: 1100
+    },
+    {
+        name: 'Google',
+        description: 'Lorem ipsum dolor sit amet, sea id molestie eleifend moderatius, ubique mandamus nec ex. Pro error mentitum ad, pri sonet facilisis voluptatum in, pri voluptua partiendo intellegebat eu.',
+        shortDescription: 'Lorem ipsum dolor sit amet, sea id molestie eleifend moderatius, ubique mandamus nec ex.',
+        price: 325.00,
+        thumbnail: 'http://res.cloudinary.com/gravitant/image/upload/v1413466762/providers/google.png',
         points: 1000
     }]
     return products
 });
 
-app.controller('ProductController', ['$scope', 'Products', function($scope, Products) {
+app.controller('ProductController', ['$scope', 'Products', 'SimilarProducts', function($scope, Products, SimilarProducts) {
     this.products = Products;
+    this.similarProducts = SimilarProducts;
     this.addToCart = false;
 }]);
