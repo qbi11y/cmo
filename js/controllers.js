915b88d2308d5514548461b0a889d742db31d078
qbi11y <qbi11y@gmail.com>
2014-12-22 11:47:47 -0600

    12-22-2014-11:46
    Charting updates from Brooks

--- js/controllers.js
+++ js/controllers.js
@@ -1,13 +1,13 @@
 var cmoControllers = angular.module('cmoControllers', []);
-cmoControllers.controller('ProductController', ['$scope', 'Products', 'SimilarProducts', 'ManageResponses', 'CompareItems', function($scope, Products, SimilarProducts, ManageResponses, CompareItems) {
+cmoControllers.controller('ProductController', ['$scope', 'Products', 'SimilarProducts', 'ManageResponses', 'CompareItems', function ($scope, Products, SimilarProducts, ManageResponses, CompareItems) {
     $scope.menuStore = true;
     this.products = Products;
     this.similarProducts = SimilarProducts;
     $scope.cartResponse = ManageResponses.getResponse();
     $scope.compare = CompareItems.getCompareItems();
-    $scope.setCompare = function(item, checked) {
+    $scope.setCompare = function (item, checked) {
         if (checked == true) {
-            $scope.compare.push(item);    
+            $scope.compare.push(item);
         } else {
             $scope.removeCompare(item)
             //console.log('remove item')
@@ -15,16 +15,16 @@ cmoControllers.controller('ProductController', ['$scope', 'Products', 'SimilarPr
         CompareItems.setCompareItems($scope.compare);
     }
 
-    $scope.removeCompare = function(item) {
+    $scope.removeCompare = function (item) {
         console.log(item.id);
-        for (i=0; i < $scope.compare.length; i++) {
+        for (i = 0; i < $scope.compare.length; i++) {
             if (item.id == $scope.compare[i].id) {
                 $scope.compare.splice(i, 1);
             }
         }
     }
-    
-    $scope.addToCart = function() {
+
+    $scope.addToCart = function () {
         //add to cart
         //on success set response;
         ManageResponses.setResponse('cart', Math.round(Math.random()));
@@ -33,60 +33,122 @@ cmoControllers.controller('ProductController', ['$scope', 'Products', 'SimilarPr
     }
 }]);
 
-cmoControllers.controller('ManageController', ['$scope', 'ManageViews', 'Applications', 'Resources', function($scope, ManageViews, Applications, Resources){
+cmoControllers.controller('ManageController', ['$scope', 'ManageViews', 'Applications', 'Resources', function ($scope, ManageViews, Applications, Resources) {
     $scope.menuManage = true;
     $scope.tabToShow = ManageViews.getTab();
     $scope.applications = Applications;
     $scope.resources = Resources;
     console.log(Resources);
-    $scope.showDetails = function(tab) {
+    $scope.showDetails = function (tab) {
         ManageViews.setTab(tab)
         $scope.tabToShow = ManageViews.getTab();
         console.log($scope.tabToShow);
-    }
+    };
+
+    $scope.options = {
+        chart: {
+            type: 'pieChart',
+            height: 100,
+            width: 100,
+            x: function (d) { return d.key; },
+            y: function (d) { return d.y; },
+            showLabels: false,
+            transitionDuration: 500,
+            labelThreshold: 0.01,
+            showLegend: false,
+            margin: { top: 0, right: 0, bottom: 0, left: 0 },
+            padding: { top: 0, right: 0, bottom: 0, left: 0 },
+            legend: {
+                margin: {
+                    top: 0,
+                    right: 0,
+                    bottom: 0,
+                    left: 0
+                }
+            }
+        }
+    };
+
+    $scope.sampledata = [
+            {
+                key: "One",
+                y: 5
+            },
+            {
+                key: "Two",
+                y: 2
+            },
+            {
+                key: "Three",
+                y: 9
+            },
+            {
+                key: "Four",
+                y: 7
+            },
+            {
+                key: "Five",
+                y: 4
+            },
+            {
+                key: "Six",
+                y: 3
+            },
+            {
+                key: "Seven",
+                y: .5
+            }
+    ];
+
+    //checks resource for data, if no data use a sample data set
+    $scope.getData = function (resource) {
+        if (resource.chartdata) return resource.chartdata;
+        return $scope.sampledata;
+    };
+
 }]);
 
-cmoControllers.controller('CatalogController', ['$scope', 'Products', function($scope, Products) {
+cmoControllers.controller('CatalogController', ['$scope', 'Products', function ($scope, Products) {
     $scope.items = Products;
     $scope.menuCatalog = true;
 
-    $scope.rowClick = function() {
+    $scope.rowClick = function () {
         console.log('enter the row')
     }
 }]);
 
-cmoControllers.factory('ManageViews', function(){
+cmoControllers.factory('ManageViews', function () {
     var tabToShow = 'resources';
     return {
-        getTab: function() {
+        getTab: function () {
             return tabToShow
         },
-        setTab: function(tab) {
+        setTab: function (tab) {
             tabToShow = tab;
         }
     }
 });
 
-cmoControllers.factory('ManageResponses', function() {
+cmoControllers.factory('ManageResponses', function () {
     var addToCartResponse;
     console.log(addToCartResponse);
     return {
-        getResponse: function() {
+        getResponse: function () {
             return addToCartResponse
         },
-        setResponse: function(action, response) {
+        setResponse: function (action, response) {
             addToCartResponse = response;
         }
     }
 });
 
-cmoControllers.factory('CompareItems', function() {
+cmoControllers.factory('CompareItems', function () {
     var compareItems = [];
     return {
-        getCompareItems: function() {
+        getCompareItems: function () {
             return compareItems;
         },
-        setCompareItems: function(items) {
+        setCompareItems: function (items) {
             compareItems = items;
         }
     }
--- js/manage-scripts.js
+++ js/manage-scripts.js
@@ -1,4 +1,4 @@
-var app = angular.module('CMO', ['ngRoute', 'cmoControllers', 'Data']);
+var app = angular.module('CMO', ['ngRoute', 'cmoControllers', 'Data', 'nvd3']);
 
 app.config(['$routeProvider', 
     function($routeProvider) {
